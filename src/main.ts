8ad7632c2bdb293bc45f26646456302b4ca59995
pc <alangerman98@yahoo.com.ar>
2026-02-06 19:11:38 -0300

    configuracion de vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+const server = express();
+
+export const createServer = async () => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
+  app.enableCors();
+  await app.init();
+  return server;
+};
+
+createServer();
