709878b637e68358c4f8874e46710dc912b56154
pc <alangerman98@yahoo.com.ar>
2026-02-20 17:28:44 -0300

    reconfiguracion del proyecto

--- src/ping/ping.controller.ts
+++ src/ping/ping.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 
+<<<<<<< HEAD
 
 @Controller()
+=======
+@Controller('auth')
+>>>>>>> bf5b756 (reconfiguracion del proyecto)
 export class PingController {
     @Get('ping')
     ping(){
